992f705e289e656afe2158ddaef49e92e72cb2c0
mm1995tk <jimi.woodstock1995@gmail.com>
2023-01-21 19:46:46 +0900

    putsの実装

--- src/Evaluator/Builtin.hs
+++ src/Evaluator/Builtin.hs
@@ -2,7 +2,9 @@ module Evaluator.Builtin (builtinFns) where
 
 import qualified Data.Map as M
 import qualified Data.Text as T
+import Debug.Trace (trace)
 import Evaluator.Type
+import Support.TypeClass (Display (display))
 import Prelude hiding (head, tail)
 import qualified Prelude
 
@@ -13,6 +15,7 @@ builtinFns =
     , push
     , head
     , tail
+    , puts
     ]
   where
     toTupleList :: [BuiltinFn] -> [(T.Text, MonkeyValue)]
@@ -25,7 +28,7 @@ len = BuiltinFn{..}
     cntOfParams = 1
     fn [LiteralValue (MonkeyArr arr)] = Right . LiteralValue . MonkeyInt . length $ arr
     fn [LiteralValue (MonkeyStr str)] = Right . LiteralValue . MonkeyInt . length . T.unpack $ str
-    fn _ = Left $ NotImpl
+    fn _ = Left NotImpl
 
 head :: BuiltinFn
 head = BuiltinFn{..}
@@ -33,7 +36,7 @@ head = BuiltinFn{..}
     name = "head"
     cntOfParams = 1
     fn [LiteralValue (MonkeyArr arr@(_ : _))] = Right . LiteralValue . Prelude.head $ arr
-    fn _ = Left $ NotImpl
+    fn _ = Left NotImpl
 
 tail :: BuiltinFn
 tail = BuiltinFn{..}
@@ -41,7 +44,7 @@ tail = BuiltinFn{..}
     name = "tail"
     cntOfParams = 1
     fn [LiteralValue (MonkeyArr arr@(_ : _))] = Right . LiteralValue . MonkeyArr . Prelude.tail $ arr
-    fn _ = Left $ NotImpl
+    fn _ = Left NotImpl
 
 push :: BuiltinFn
 push = BuiltinFn{..}
@@ -49,4 +52,17 @@ push = BuiltinFn{..}
     name = "push"
     cntOfParams = 2
     fn [LiteralValue (MonkeyArr arr), LiteralValue a] = Right . LiteralValue . MonkeyArr . reverse $ (a : reverse arr)
-    fn _ = Left $ NotImpl
+    fn _ = Left NotImpl
+
+puts :: BuiltinFn
+puts = BuiltinFn{..}
+  where
+    name = "puts"
+    cntOfParams = 1
+    fn [a] = do
+      -- TODO: traceを使わないように修正する
+      !_ <- pure $ trace (displayer a) a
+      return $ LiteralValue MonkeyNull
+    fn _ = Left NotImpl
+    displayer (LiteralValue a) = display a
+    displayer (ReturnValue a) = display a
